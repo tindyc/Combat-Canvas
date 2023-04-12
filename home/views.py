c7420214ae94a7b71d0aaebd8fbea10cba4cf3ae
Tindy Chan <tindychan@Tindys-MacBook-Pro.local>
2023-04-12 19:03:53 +0100

    Create Home App

---
+++ home/views.py
@@ -0,0 +1,12 @@
+"""
+Home Views
+"""
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')

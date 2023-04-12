0db30d726585eb9f0f1fca33e5bd7b44b24bd674
Tindy Chan <tindychan@Tindys-MacBook-Pro.local>
2023-04-12 21:51:03 +0100

    Set up static files and install whitenoise

--- CombatCanvas/settings.py
+++ CombatCanvas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3q2$uw*+n&@^qbrvq1(a-r(&ym_kpsyo4n=-ep5hq66z%h-n@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,7 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+"""HOME URLs"""
 from django.urls import path
 from . import views
 

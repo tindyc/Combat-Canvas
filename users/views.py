6c935c575b8e88c8184129903d5ae7f758feb025
Tindy Chan <tindychan@Tindys-MacBook-Pro.local>
2023-04-13 17:19:42 +0100

    Render User profiles

--- users/views.py
+++ users/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Profile
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    context = {'profile': profile}
+    return render(request, 'users/user-profile.html', context)

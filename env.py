5ee51b65880f05413ec138c55e7d594c99e55d08
Tindy Chan <tindychan@Tindys-MacBook-Pro.local>
2023-04-15 09:24:14 +0100

    Deploy settings and update nav

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["SECRET_KEY"] = "django-insecure-3q2$uw*+n&@^qbrvq1(a-r(&ym_kpsyo4n=-ep5hq66z%h-n@*"
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIAXSRZHWTBKB5ISKF7"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
+os.environ["DEVELOPMENT"] = "True"
+#os.environ["DATABASE_URL"] = "postgresql://postgres:IeoPiDlq4s27VYm2yWVk@containers-us-west-190.railway.app:7008/railway"
